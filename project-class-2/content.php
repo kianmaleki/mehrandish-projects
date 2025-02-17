0b554cce08bdbb081fd119ac322707c94f080ddf
kian <150921250+kianmaleki@users.noreply.github.com>
2025-02-17 14:06:38 +0330

    Add content.php

---
+++ project-class-2/content.php
@@ -0,0 +1,8 @@
+<form action="register.php" method="post">
+    <input type="text" placeholder="realname" name="realname">
+    <input type="text" placeholder="username" name="username">
+    <input type="password" placeholder="password" name="password">
+    <input type="password" placeholder="repassword" name="repassword">
+    <input type="email" placeholder="email" name="email">
+    <input type="submit" placeholder="submit" name="submit">
+</form>
